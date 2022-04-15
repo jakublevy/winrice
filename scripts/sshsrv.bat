322bee923ba907d4ef45c823b8245d0df3de8b02
Jakub Levý <jakub.levy15@gmail.com>
2022-04-15 14:14:11 +0200

    srv ssh script kodi change

--- scripts/sshsrv.bat
+++ scripts/sshsrv.bat
@@ -1,7 +1,6 @@
 @echo off
 setlocal
 
-for /f "tokens=2,3 delims=\" %%a in ("%KodiDrivePath%") do ( set "pt=%%a" )
-for /f "tokens=2,3 delims=@" %%b in ("%pt%") do ( set "dst=%%b" )
+for /f "tokens=1,2 delims=\" %%a in ("%KodiDrivePath%") do ( set "dst=%%a" )
 ssh yadari@%dst% -p %SrvSshPort%
 endlocal
